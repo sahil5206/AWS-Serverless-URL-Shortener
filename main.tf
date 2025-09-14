8a3f62ab9a75b4b25e4db63de2fc1b12295e19ff
sahil gupta <Sahilg5206official@gmail.com>
2025-09-14 14:34:13 +0530

    added to main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,49 @@ provider "aws"{
     region = "ap-south-1"
 }
 
+#DynamoDB table
+
+resource "dynamo_db_table" "url_table" {
+    name = "urlTable"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "shortId"
+
+    attribute {
+        name = "shortId"
+        type = "S"
+    }
+}
+
+#IAM roles
+
+resource "aws_iam_role" "lambda_role" {
+    name = "lambda_url_shortener_role"
+
+    assure_role_policy = jsonencode({
+        Version = 2012-10-17"
+        Statement = [{
+            Action = "sts:Asumerole"
+            Effect = "Allow"
+            Principal = {
+                Service = "lambda.amazonaws.com"
+            }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachement" "lambda_basic" {
+    role = aws_iam_role.lambda_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachement" "lambda_dynamo" {
+    role = aws_iam_role.lambda_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+#lambda: create shortener 
+
+data "archieve_file" "create_zip" {
+    type = "zip"
+    
+}
